8ad0f09d0666707ca5866aa828f14bdae9fda687
shadow <pieroramirez810@gmail.com>
2023-07-12 21:58:23 -0400

    test: add first test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   ...(process.env.BASE_URL ? { base: process.env.BASE_URL } : {}),
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
 });
