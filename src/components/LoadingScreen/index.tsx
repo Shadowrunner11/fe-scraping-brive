6637a0ae91de59c3a98ca9d312f943eb8dd1b681
shadow <pieroramirez810@gmail.com>
2023-07-13 00:44:48 -0400

    fix: conditional render

--- src/components/LoadingScreen/index.tsx
+++ src/components/LoadingScreen/index.tsx
@@ -7,7 +7,7 @@ export const LoadingScreen = () => (
       <svg
         xmlns='http://www.w3.org/2000/svg'
         width='300'
-        height='127'
+        height='128'
         viewBox='0 0 300 128'
         fill='none'
       >
